500d63b7ea2658a0488ab75933229c2e1b45742c
Eckhoee <ddavidbailey@outlook.com>
2021-11-19 22:01:16 -0500

    Updated Code

---
+++ src/ParityBit.java
@@ -0,0 +1,26 @@
+public class ParityBit {
+    /*
+        Name: David Bailey
+        ID: 6675482
+        Date: November 19, 2021
+     */
+
+    /*
+        1. Even number of ones, parity bit is one
+        2. Odd number of ones, parity bit is zero
+        3. Should maintain an odd number of ones from the original 8 bits when applying the parity bit (odd parity)
+            -Standard PC memory is odd parity
+        4. If there are an odd number of ones and parity bit is set to one, system will be looking for even parity
+
+
+        IMPLMENTATION: Train feed-forward neural net with back prop. to act as a parity checking system
+     */
+    public ParityBit(){
+
+    }
+
+    public static void main(String[] args) {
+        new ParityBit();
+    }
+
+}
